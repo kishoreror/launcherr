0b0cdf707ea9a84567181989251f95adeca6b273
kishoreror <kishoreror@gmail.com>
2014-06-22 11:56:59 +0530

    seconds yar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
- has_attached_file :coverimage ,:styles => { :medium => "300x300>", :thumb => "100x100>" }
- has_attached_file :logoimage ,:styles => { :medium => "300x300>", :thumb => "100x100>" }
- validates_attachment :coverimage, content_type: { content_type: /\Aimage\/.*\Z/ }
- validates_attachment :logoimage, content_type: { content_type: /\Aimage\/.*\Z/ }
+ has_attached_file :coverimage 
+ has_attached_file :logoimage 
 
+ validates_attachment :coverimage, content_type: { content_type: /\Aimage\/.*\Z/ }
+ validates_attachment :logoimage, content_type: { content_type: /\Aimage\/.*\Z/ } 
  validates_formatting_of :youtube_id, using: :url
   acts_as_votable 
 
