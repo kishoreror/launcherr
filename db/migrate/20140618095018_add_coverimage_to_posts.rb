901c6a026823a45cedbccddebf0ea89298e084c7
jakebaird <jakebaird89@gmail.com>
2014-06-23 15:47:57 +1000

    Commit 23-6

--- db/migrate/20140618095018_add_coverimage_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddCoverimageToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :coverimage, :string
-  end
-end
--- db/migrate/20140618095043_add_logoimage_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddLogoimageToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :logoimage, :string
-  end
-end
